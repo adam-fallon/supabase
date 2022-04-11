923a1091fa4d8e56c863bd38df9f0b7d9228a3d5
TzeYiing <ty@tzeyiing.com>
2022-04-11 11:54:32 +0200

    add auth constants

--- studio/components/interfaces/Settings/Logs/Logs.constants.ts
+++ studio/components/interfaces/Settings/Logs/Logs.constants.ts
@@ -229,6 +229,9 @@ export const SQL_FILTER_TEMPLATES: any = {
     'severity.info': `metadata.level = 'info'`,
     'severity.debug': `metadata.level = 'debug'`,
   },
+  auth_logs: {
+    ..._SQL_FILTER_COMMON,
+  },
 }
 
 // export const cleanQuery = (str: string) => str.replaceAll(/\n/g, ' ')
@@ -240,6 +243,7 @@ export enum LogsTableName {
   POSTGRES = 'postgres_logs',
   FUNCTIONS = 'function_logs',
   FN_EDGE = 'function_edge_logs',
+  AUTH = 'auth_logs',
 }
 
 export const LOGS_TABLES = {
@@ -247,6 +251,7 @@ export const LOGS_TABLES = {
   database: LogsTableName.POSTGRES,
   functions: LogsTableName.FUNCTIONS,
   fn_edge: LogsTableName.FN_EDGE,
+  auth: LogsTableName.AUTH
 }
 
 export const LOGS_SOURCE_DESCRIPTION = {
@@ -254,6 +259,7 @@ export const LOGS_SOURCE_DESCRIPTION = {
   [LogsTableName.POSTGRES]: 'Database logs obtained directly from Postgres.',
   [LogsTableName.FUNCTIONS]: 'Function logs generated from runtime execution.',
   [LogsTableName.FN_EDGE]: 'Function call logs, containing the request and response.',
+  [LogsTableName.AUTH]: 'Authentication logs from GoTrue',
 }
 
 export const genCountQuery = (table: string): string => `SELECT count(*) as count FROM ${table}`
